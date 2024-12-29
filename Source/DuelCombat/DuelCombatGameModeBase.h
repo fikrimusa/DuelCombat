17ef10a53394023ae6f82a2f1a73dfed610be5f4
Fikri Musa <nurfikrimusa@gmail.com>
2024-12-29 23:29:40 +0800

    Added Game Mode Base

---
+++ Source/DuelCombat/DuelCombatGameModeBase.h
@@ -0,0 +1,20 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "DuelCombatGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DUELCOMBAT_API ADuelCombatGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
