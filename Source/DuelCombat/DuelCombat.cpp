46228e3a8abca34be2a2001041ac0f976a9c5f8b
Fikri Musa <nurfikrimusa@gmail.com>
2024-12-29 23:24:58 +0800

    Add project files.

---
+++ Source/DuelCombat/DuelCombat.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "DuelCombat.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, DuelCombat, "DuelCombat" );
